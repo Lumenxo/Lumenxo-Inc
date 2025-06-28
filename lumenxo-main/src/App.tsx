f4daf85e095920ad7a7f4dd02494f15f90c123c2
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2025-06-29 01:24:10 +0530

    ui: Replace with work in progress

--- lumenxo-main/src/App.tsx
+++ lumenxo-main/src/App.tsx
@@ -1,16 +1,24 @@
 import React, { useEffect } from 'react';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
-import { AnimatePresence } from 'framer-motion';
-import { Loader } from './components/Loader';
-import { useGlobalContext } from './context/GlobalContext';
-import { Cursor } from './components/ui/Cursor';
-import { ParticleBackground } from './components/background/ParticleBackground';
-import { BackToTop } from './components/ui/BackToTop';
-import GridBackground from './components/background/GridBackground';
+//import { RouterProvider } from 'react-router-dom';
+//import { router } from './routes';
+//import { AnimatePresence } from 'framer-motion';
+//import { Loader } from './components/Loader';
+//import { useGlobalContext } from './context/GlobalContext';
+//import { Cursor } from './components/ui/Cursor';
+//import { ParticleBackground } from './components/background/ParticleBackground';
+//import { BackToTop } from './components/ui/BackToTop';
+//import GridBackground from './components/background/GridBackground';
 
 const App: React.FC = () => {
-  const { isLoading, setIsLoading } = useGlobalContext();
+  //const { isLoading, setIsLoading } = useGlobalContext();
+  
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <h1> Work in progress ...</h1>
+      <br />
+      <p> Working to be more awesome ... 😎 </p>
+    </div>
+  )
 
   useEffect(() => {
     setTimeout(() => {
